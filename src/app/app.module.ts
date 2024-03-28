81dda4d2c4455c522215a26710649650b7334f99
Felipe <61094586+nascimento-felipe@users.noreply.github.com>
2024-03-28 18:36:22 -0300

    build: Adiciona o modulo DragAndDrop no sistema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './pages/main-view/main-view.component';
 
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainViewComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainViewComponent],
+  imports: [BrowserModule, AppRoutingModule, DragDropModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
